b15a17973e511f545f52eca8a42160dd2f493977
King-David <ajanakingdavid@outlook.com>
2025-08-26 16:34:21 +0100

    'Models'

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ y = data.iloc[:, -1].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 
-with mlflow.start_run():
+with mlflow.start_run(run_name='Random Forest'):
     mlflow.autolog()
     pipeline = make_pipeline(StandardScaler(),
                             RandomForestClassifier()
@@ -40,24 +40,25 @@ with mlflow.start_run():
     with open("classification_report.txt", "w") as f:
         f.write(report_str)
     mlflow.log_artifact("classification_report.txt")
-    #mlflow.sklearn.log_model(pipeline, name="model", signature=signature)
-    joblib.dump(pipeline, "pipeline.pkl")
-    mlflow.log_artifact("pipeline.pkl")
+    #mlflow.sklearn.log_model(pipeline, "rf_model", signature=signature)
+    #joblib.dump(pipeline, "pipeline.pkl")
+    #mlflow.log_artifact("pipeline.pkl")
 
-with mlflow.start_run():
+with mlflow.start_run(run_name='Logistic Regression'):
     mlflow.autolog()
     pipeline2 = make_pipeline(
         StandardScaler(),
         LogisticRegression()
     )
+    pipeline2.fit(X_train, y_train)
     prediction = pipeline2.predict(X_test)
     sample_data = X[:10]
-    sample_prediction = pipeline.predict(sample_data)
+    sample_prediction = pipeline2.predict(sample_data)
     signature2 = infer_signature(sample_data, sample_prediction)
     report_str_lr = classification_report(y_test, prediction)
     with open("lr_classification_report.txt", "w") as f:
         f.write(report_str_lr)
     mlflow.log_artifact("lr_classification_report.txt")
-    joblib.dump(pipeline2, "lr_model.pkl")
-    mlflow.log_artifact("lr_model.pkl")
-    #mlflow.sklearn.log_model(pipeline2, name="lr_model", signature=signature2)
+    #joblib.dump(pipeline2, "lr_model.pkl")
+    #mlflow.log_artifact("lr_model.pkl")
+    #mlflow.sklearn.log_model(pipeline2, "lr_model", signature=signature2)
